c3ce1c2fdf66012603e683877e09834a7e4e265b
OCrespoJ <oscar.crespo.024@gmail.com>
2018-01-22 06:06:00 -0500

    API final

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=SleepHoney',
+			'dsn'        => 'mysql:host=localhost;dbname=honey',
 			'username'   => 'honey',
 			'password'   => 'gyRzmnSpVrrNB9Bl',
 		),
